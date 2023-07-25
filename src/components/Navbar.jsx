f04df082596cf104f65573d0439086fd79cc36f0
KLS <100925067+IKLSI@users.noreply.github.com>
2023-07-25 13:22:50 +0200

    Modification Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { BiSearch } from "react-icons/bi";
 
+const items = ["Profil", "Projet", "Langage"];
+
 export default function Navbar() {
-	return (
-		<Nav>
-		<div className="title">
-			<h1>
-			Voici mon <span>DASHBOARD GITHUB</span>
-			</h1>
-		</div>
-		<div className="search">
-			<BiSearch />
-			<input type="text" placeholder="Rechercher" />
-		</div>
-		</Nav>
-	);
+const [isListVisible, setIsListVisible] = useState(false);
+const [searchInput, setSearchInput] = useState("");
+const filteredItems = items.filter(
+	(item) =>
+	item.toLowerCase().indexOf(searchInput.toLowerCase()) !== -1
+);
+
+const handleSearchClick = () => {
+	setIsListVisible(!isListVisible);
+};
+
+const handleInputChange = (event) => {
+	setSearchInput(event.target.value);
+	setIsListVisible(true);
+};
+
+return (
+	<Nav>
+	<div className="title">
+		<h1>
+		Voici mon <span>DASHBOARD GITHUB</span>
+		</h1>
+	</div>
+	<div className="search" onClick={handleSearchClick}>
+		<BiSearch />
+		<input
+		type="text"
+		placeholder="Rechercher"
+		value={searchInput}
+		onChange={handleInputChange}
+		/>
+		{isListVisible && filteredItems.length > 0 && (
+		<ul className="search-list">
+		{filteredItems.map((item, index) => (
+			<li key={index}>{item}</li>
+		))}
+		</ul>
+	)}
+	</div>
+	</Nav>
+);
 }
 
 const Nav = styled.nav`
@@ -40,9 +70,6 @@ const Nav = styled.nav`
 		gap: 1rem;
 		padding: 1rem 8rem 1rem 1rem;
 		border-radius: 1rem;
-			svg {
-			color: #ffc107;
-			}
 			input {
 			background-color: transparent;
 			border: none;
@@ -55,6 +82,22 @@ const Nav = styled.nav`
 			&::placeholder {
 				color: #ffc107;
 				font-family: "Permanent Marker", cursive;
+			}
+		}
+	}
+
+	.search-list {
+		gap: 1rem;
+		display: flex;
+		list-style: none;
+		font-family: cursive;
+		font-weight: bold;
+		color: #ffc107;
+
+		li {
+			padding: 0.5rem 1rem;
+			&:hover {
+				color: cyan;
 			}
 		}
 	}
